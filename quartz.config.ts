c7597f742c1abe4da1523841f18c1a6b7cf618f5
JSaroni <92460229+JoBrew32@users.noreply.github.com>
2025-12-23 17:40:15 -0600

    Update quartz.config.ts
    Added base url.

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "jobrew32.github.io/brewsters-burrow/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
