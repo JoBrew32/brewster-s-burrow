f9874e63c1e622c71af8a9d4eead5827391b709f
JSaroni <92460229+JoBrew32@users.noreply.github.com>
2025-12-29 23:46:07 -0600

    Update Explorer.tsx

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -84,7 +84,7 @@ export default ((userOpts?: Partial<Options>) => {
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="14"
+            width="24"
             height="24"
             viewBox="0 0 24 24"
             stroke-width="2"
